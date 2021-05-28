91bdadee117d5aa6117ad9c4743cfc087d0cd6d8
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-05-29 00:07:07 +0700

    delete comment i think 🤔

--- index.js
+++ index.js
@@ -13,10 +13,8 @@ function padLeadingZeros(num, size) {
 }
 
 app.get('/', (req, res) => {
-    //var fileDelete = null;
     try {
         if(req.query.fresh !== undefined){
-            //fileDelete = fs.unlinkSync('tmp/'+req.query.date+'.txt');
             fs.unlinkSync('tmp/'+req.query.date+'.txt');
         }
     } catch (err) {
@@ -37,8 +35,6 @@ app.get('/', (req, res) => {
         .then((body) => {
             let data = [["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481",0],["\u0e40\u0e25\u0e02\u0e2b\u0e19\u0e49\u0e323\u0e15\u0e31\u0e27",0,0],["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e223\u0e15\u0e31\u0e27",0,0],["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e222\u0e15\u0e31\u0e27",0],["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e02\u0e49\u0e32\u0e07\u0e40\u0e04\u0e35\u0e22\u0e07\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481",0,0],["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e482",0,0,0,0,0],["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e483",0,0,0,0,0,0,0,0,0,0],["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e484",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e485",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
             let $ = cheerio.load(body)
-        
-            //console.log($('strong').toArray())
 
             if(req.query.from !== undefined){
                 switch (req.query.date.substr(2, 2)){
@@ -68,22 +64,11 @@ app.get('/', (req, res) => {
             data[4][1] = $('strong').toArray()[6].firstChild.data
             data[4][2] = $('strong').toArray()[7].firstChild.data
 
-            /*for (const type of $('strong').toArray()) {
-                var arrit = type.attribs.class + ''
-                if(!arrit.search('lotto__number')){
-                    //console.log(type.attribs.class)
-                    console.log(type.firstChild.data)
-                    data[0][1] = 
-                }
-            }*/
-
             let k = 5
             let i = 1
             for (const type of $('span').toArray()) {
                 var arrit = type.attribs.class + ''
                 if(!arrit.search('lotto__number')){
-                    //console.log(type.attribs.class)
-                    //console.log(type.firstChild.data)
                     if(k == 5 && i <= 5){
                         data[k][i] = type.firstChild.data
                         i++
@@ -113,7 +98,6 @@ app.get('/', (req, res) => {
             }
             fs.writeFile('tmp/'+req.query.date+'.txt', JSON.stringify(data), function (err) {
                 if (err) throw err;
-                //console.log('Saved!');
                 res.send(data)
             });
         })
@@ -143,10 +127,8 @@ app.get('/index2', (req, res) => {
       case '11' : monthtext="พฤศจิกายน"; break;
       case '12' : monthtext="ธันวาคม"; break;
     }
-    //var fileDelete = null;
     try {
         if(req.query.fresh !== undefined){
-            //fileDelete = fs.unlinkSync('tmp/'+req.query.date+'.txt');
             fs.unlinkSync('tmp/'+req.query.date+'.txt');
         }
     } catch (err) {
@@ -170,8 +152,6 @@ app.get('/index2', (req, res) => {
             if(req.query.from !== undefined){
                 data[0][0] = req.query.date.substring(0, 2)+monthtext+req.query.date.substring(4, 8)
             }
-
-            //console.log($('b').toArray())
 
             if($('b').toArray()[2] == null){
                 res.send(data)
@@ -203,11 +183,6 @@ app.get('/index2', (req, res) => {
             let maxwave = 5;
 
             for (const type of $('div').toArray()) {
-                //var arrit = type.attribs.class + ''
-                //if(!arrit.search('lotto__number')){
-                    //console.log(type.attribs.class)
-                    //console.log(type.firstChild.data)
-                //}
                 if(type.attribs.class == 'ltr_dc ltr-fx ltr_c20'){
                     if (minwave < maxwave) {
                         minwave++;
@@ -231,11 +206,8 @@ app.get('/index2', (req, res) => {
                 }
             }
 
-            //console.log(data)
-            //res.send('test')
             fs.writeFile('tmp/'+req.query.date+'.txt', JSON.stringify(data), function (err) {
                 if (err) throw err;
-                //console.log('Saved!');
                 res.send(data)
             });
         });
@@ -267,10 +239,7 @@ app.get('/god', async (req, res) => {
     try {
         fileContents = fs.readFileSync('tmp/cache.txt');
     } catch (err) {
-        // Here you get the error when the file was not found,
-        // but you also get any other error
     }
-    //console.log(fileContents)
     if (fileContents) {
         jdata = JSON.parse(fileContents);
         if (
@@ -278,26 +247,21 @@ app.get('/god', async (req, res) => {
             new Date().getFullYear() + 543
         ) {
             year = new Date().getFullYear() + 543;
-            //console.log("yes this year");
-            //console.log(year)
+        }else{
+            year = jdata[jdata.length - 1].substring(4, 8)
         }
         jdata.forEach(function (value, i) {
             if (
                 value.substring(4, 8) ==
-                new Date().getFullYear() + 543
+                year
             ) {
-                //delete jdata[i];
                 countloveme--;
-                //console.log('remove json this year')
             }
         });
         jdata.splice(countloveme);
     }
     let day
     while(year <= nextyear){
-        //console.log(year)
-        //console.log(nextyear)
-        //console.log('insidewhile')
         channel = []
         for (let i=0; i < 10; i++) {
             preyearsuperlist = [];
@@ -307,18 +271,13 @@ app.get('/god', async (req, res) => {
             if(ayear > nextyear){
                 break
             }
-            //console.log(ayear)
-            //console.log(year)
-            //console.log('insidefor')
             await fetch('https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%9B%E0%B8%B5-'+ayear+'.aspx')
             .then(res => res.text())
             .then((body) => {
                 var $ = cheerio.load(body);
-                //let $ = cheerio.load(body)
                 for (const val of $('font').toArray()){
                     if(val.firstChild.data.indexOf('ตรวจสลากกินแบ่งรัฐบาล') > -1){
                         day = val.firstChild.data.split(" ").splice(2)
-                        //console.log(day)
                         let monthnum
                         switch (day[2]){
                             case 'มกราคม' : monthnum="01"; break;
@@ -339,8 +298,6 @@ app.get('/god', async (req, res) => {
                     }
                 }
                 for (const val of peryear){
-                    //console.log('insideforval')
-                    //console.log(val)
                     yearlist.push(val)
                     if(jdata){
                         jdata.push(val)
@@ -350,7 +307,6 @@ app.get('/god', async (req, res) => {
                     preyearlist.push(val)
                     try {
                         if(day[3] == new Date().getFullYear() + 543){
-                            //fileDelete = fs.unlinkSync('tmp/'+req.query.date+'.txt');
                             fs.unlinkSync('tmp/'+req.query.date+'.txt');
                             console.log('yes this year')
                         }
@@ -359,26 +315,20 @@ app.get('/god', async (req, res) => {
                     }
                     fs.writeFile('tmp/'+day[3]+'.txt', JSON.stringify(preyearlist), function (err) {
                         if (err) throw err;
-                        //console.log('Saved!');
                     });
                 }
             })
         }
-        //console.log(year)
-        //console.log(yearlist)
-        //console.log(jdata)
         year += 10
     }
     if(jdata){
         fs.writeFile('tmp/cache.txt', JSON.stringify(jdata), function (err) {
             if (err) throw err;
-            //console.log('Saved!');
             res.send(jdata)
         });
     }else{
         fs.writeFile('tmp/cache.txt', JSON.stringify(yearlist), function (err) {
             if (err) throw err;
-            //console.log('Saved!');
             res.send(yearlist)
         });
     }
@@ -388,10 +338,8 @@ app.get('/gdpy', (req, res) => {
     let peryear = []
     let yearlist = []
     var fileContents = null;
-    //var fileDelete = null;
     try {
         if(req.query.year == new Date().getFullYear() + 543){
-            //fileDelete = fs.unlinkSync('tmp/'+req.query.year+'.txt');
             fs.unlinkSync('tmp/'+req.query.year+'.txt');
             console.log('yes this year')
         }
@@ -432,7 +380,6 @@ app.get('/gdpy', (req, res) => {
             }
             fs.writeFile('tmp/'+req.query.year+'.txt', JSON.stringify(yearlist), function (err) {
                 if (err) throw err;
-                //console.log('Saved!');
                 res.send(yearlist)
             });
         })
@@ -446,52 +393,36 @@ app.get('/checklottery', (req, res) => {
     .then((body) => {
         body.forEach(function (val, x) {
             val.forEach(function (superval, y) {
-                //echo "$x and $y = $val<br>";
                 if(superval == req.query.search || superval == req.query.search.substr(0,3) || superval == req.query.search.substr(3,6) || superval == req.query.search.substr(4,6) && y != 0){
-                    //echo "hee";
                     if(x == 0){
-                        //echo "111111";
                         result = result+"111111,";
                     }
                     if(x == 1){
-                        //echo "111111";
                         result = result+"333000,";
                     }
                     if(x == 2){
-                        //echo "111111";
                         result = result+"000333,";
                     }
                     if(x == 3){
-                        //echo "111111";
                         result = result+"000022,";
                     }
                     if(x == 4){
-                        //echo "111112";
                         result = result+"111112,";
                     }
                     if(x == 5){
-                        //echo "222222";
                         result = result+"222222,";
                     }
                     if(x == 6){
-                        //echo "333333";
                         result = result+"333333,";
                     }
                     if(x == 7){
-                        //echo "444444";
                         result = result+"444444,";
                     }
                     if(x == 8){
-                        //echo "555555";
                         result = result+"555555,";
                     }
-                    //echo $superval;
                 }
-                //echo $superval;
             })
-            /*if($lot_array[0][1] == req.query.search){
-                echo "hee";
-            }*/
         })
         res.send(result.substring(0, result.length - 1))
     })
@@ -543,7 +474,6 @@ app.get('/getchit', (req, res) => {
 
 app.get('/finddol', async (req, res) => {
     let channels
-    let count = 0
     let allwin = []
     await fetch('http://localhost:'+port+'/god')
     .then(res => res.json())
@@ -551,7 +481,6 @@ app.get('/finddol', async (req, res) => {
         channels = body.splice(408)
     })
     for (const val of channels) {
-        //console.log(val)
         await fetch('http://localhost:'+port+'/?date='+val+'&from')
         .then(res => res.json())
         .then((body) => {
@@ -570,8 +499,3 @@ app.get('/finddol', async (req, res) => {
 app.listen(port, '0.0.0.0', () => {
     console.log('Example app listening at http://localhost:'+port)
 })
-
-/*$('h2.title').text('Hello there!');
-$('h2').addClass('welcome');
-
-$.html();*/
