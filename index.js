08254c2c46646a919f248974e2806ce3c62e268e
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2021-10-02 21:52:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -781,7 +781,7 @@ app.get('/finddol', async (req, res) => {
         }
         res.send(allwin)
         
-        var https = require('follow-redirects').https;
+        /*var https = require('follow-redirects').https;
 
         var options = {
             'method': 'POST',
@@ -822,7 +822,7 @@ app.get('/finddol', async (req, res) => {
 
         reqtwo.write(postData);
 
-        reqtwo.end();
+        reqtwo.end();*/
     } else {
         fetch('https://astro.meemodel.com/%E0%B8%A7%E0%B8%B4%E0%B9%80%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%B0%E0%B8%AB%E0%B9%8C%E0%B9%80%E0%B8%A5%E0%B8%82%E0%B8%AB%E0%B8%A7%E0%B8%A2/' + req.query.search, { redirect: 'error' })
             .then(res => res.text())
