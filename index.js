70283f789a935a09953d75eb28b07eb94330f0b2
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-11-06 19:00:36 +0700

    ok

--- index.js
+++ index.js
@@ -597,23 +597,29 @@ app.get('/index2', (req, res) => {
 
 app.get('/index3', (req, res) => {
     var raw = JSON.stringify({
-        "date": "01",
-        "month": "11",
-        "year": "2021"
+        date: "01",
+        month: "11",
+        year: "2021"
     });
 
     var requestOptions = {
         method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(raw),
+        headers: { "Content-Type": "application/json"},
+        body: raw,
         redirect: 'follow'
     };
 
-    fetch("https://cors-anywhere.herokuapp.com/https://www.glo.or.th/api/lottery/getLotteryAward", requestOptions)
+    fetch("https://www.glo.or.th/api/lottery/getLotteryAward", requestOptions)
         .then(response => response.json())
         .then(result => {
             let data = [["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481", 0],["\u0e40\u0e25\u0e02\u0e2b\u0e19\u0e49\u0e323\u0e15\u0e31\u0e27", 0, 0],["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e223\u0e15\u0e31\u0e27", 0, 0],["\u0e40\u0e25\u0e02\u0e17\u0e49\u0e32\u0e222\u0e15\u0e31\u0e27", 0],["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e02\u0e49\u0e32\u0e07\u0e40\u0e04\u0e35\u0e22\u0e07\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e481", 0, 0],["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e482", 0, 0, 0, 0, 0],["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e483", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e484", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],["\u0e23\u0e32\u0e07\u0e27\u0e31\u0e25\u0e17\u0e35\u0e485", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
             data[0][1] = result["response"]["data"]["first"]["number"][0]["value"]
+            /*result["response"]["data"]["second"]["number"].forEach(element => {
+                
+            });*/
+            for (let [index, val] of result["response"]["data"]["second"]["number"]) {
+                data[5][index+1] = val["value"]
+            }
             res.send(data)
         })
         .catch(error => console.log('error', error));
