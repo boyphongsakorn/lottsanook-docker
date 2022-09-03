b6b9a46e69f0c9a92e3b1fd9d85f8ca652585d3d
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-09-03 15:29:01 +0700

    02

--- newindex.js
+++ newindex.js
@@ -188,7 +188,7 @@ fastify.get('/', async (request, reply) => {
                     test = data
                 } else {
                     var date = new Date(parseInt(request.query.date.substr(4, 4)) - 543, parseInt(request.query.date.substr(2, 2)) - 1, parseInt(request.query.date.substr(0, 2)) + 1);
-                    var thatdate = new Date(2010, 02 - 1, 16 + 1);
+                    var thatdate = new Date(2010, 2 - 1, 16 + 1);
                     console.log(date)
                     console.log(thatdate)
                     if (date.getTime() === thatdate.getTime() || date < thatdate) {
